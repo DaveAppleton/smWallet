f7e4712dc0c9729a8aebc667b416cfb0984126c3
Dave Appleton <calistralabs@gmail.com>
2020-08-04 23:08:37 +0800

    First Cut

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/DaveAppleton/smWallet
+
+go 1.14
+
+require (
+	github.com/davecgh/go-xdr v0.0.0-20161123171359-e6a2ba005892
+	github.com/ethereum/go-ethereum v1.9.15
+	github.com/onsi/gomega v1.10.1 // indirect
+	github.com/spacemeshos/ed25519 v0.0.0-20200604074309-d72da3b5f487
+	github.com/spacemeshos/go-spacemesh v0.1.12
+	github.com/tyler-smith/go-bip39 v1.0.2
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
+)
---
+++ smWalletData.go
@@ -0,0 +1,74 @@
+package smWallet
+
+import (
+	"errors"
+
+	"github.com/spacemeshos/ed25519"
+	"github.com/spacemeshos/go-spacemesh/common/types"
+)
+
+// GetMnemonic returns the mnemonic string associated with the wallet
+func (w Wallet) GetMnemonic() (string, error) {
+	if !w.unlocked {
+		return "", errors.New(ErrorWalletNotUnlocked)
+	}
+	return w.Crypto.confidential.Mnemonic, nil
+}
+
+// GetNumberOfAccounts returns the number of accounts held in said wallet
+func (w Wallet) GetNumberOfAccounts() (int, error) {
+	if !w.unlocked {
+		return 0, errors.New(ErrorWalletNotUnlocked)
+	}
+	return len(w.Crypto.confidential.Accounts), nil
+}
+
+// GetAddress retrieves an address from a wallet if unlocked and it has been generated
+func (w *Wallet) GetAddress(accountNumber int) (types.Address, error) {
+	if !w.unlocked {
+		return types.Address{}, errors.New(ErrorWalletNotUnlocked)
+	}
+	if accountNumber >= len(w.Crypto.confidential.Accounts) {
+		return types.Address{}, errors.New(ErrorWalletDoesNotHaveThatAddress)
+	}
+	return w.Crypto.confidential.Accounts[accountNumber].Address(), nil
+}
+
+// GetPublicKey retrieves an address from a wallet if unlocked and it has been generated
+func (w *Wallet) GetPublicKey(accountNumber int) (ed25519.PublicKey, error) {
+	if !w.unlocked {
+		return []byte{}, errors.New(ErrorWalletNotUnlocked)
+	}
+	if accountNumber >= len(w.Crypto.confidential.Accounts) {
+		return []byte{}, errors.New(ErrorWalletDoesNotHaveThatAddress)
+	}
+	thisAccount := w.Crypto.confidential.Accounts[accountNumber]
+	private, err := thisAccount.PrivateKey()
+	if err != nil {
+		return []byte{}, err
+	}
+	return PublicKey(private), nil
+}
+
+// GetAccountDisplayName retrieves an account name from a wallet (if unlocked and account exists)
+func (w *Wallet) GetAccountDisplayName(accountNumber int) (string, error) {
+	if !w.unlocked {
+		return "", errors.New(ErrorWalletNotUnlocked)
+	}
+	if accountNumber >= len(w.Crypto.confidential.Accounts) {
+		return "", errors.New(ErrorWalletDoesNotHaveThatAddress)
+	}
+	return w.Crypto.confidential.Accounts[accountNumber].DisplayName, nil
+}
+
+// SetCurrent - set current wallet by number
+func (w *Wallet) SetCurrent(accountNumber int) error {
+	if !w.unlocked {
+		return errors.New(ErrorWalletNotUnlocked)
+	}
+	if accountNumber >= len(w.Crypto.confidential.Accounts) {
+		return errors.New(ErrorWalletDoesNotHaveThatAddress)
+	}
+	w.Crypto.confidential.accountNumber = accountNumber
+	return nil
+}
